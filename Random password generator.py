278ae49e391963e29238ea93250282318791eb33
yashvi sidapara <sidaparayashvi18@gmail.com>
2026-06-27 19:54:41 +0530

    random password generator

---
+++ Random password generator.py
@@ -0,0 +1,18 @@
+import random
+import string
+
+#print(string.ascii_letters)
+# print(string.digits)
+# print(string.punctuation)
+
+# val = random.choice(['a', 'b', 'c', 'd'])
+# print(val)
+
+
+pass_len = 12
+charValues = string.ascii_letters + string.digits + string.punctuation
+
+password = ""
+for i in range(pass_len):
+    password += random.choice(charValues)
+print("Your random password is: ", password)    
