febbdb2f6ecd77d442f358275c6f0981f367c6b6
yashvi sidapara <sidaparayashvi18@gmail.com>
2026-06-27 19:20:24 +0530

    mini random game

--- project.py
+++ project.py
@@ -1,3 +1,15 @@
-import random;
+import random
 
+target = random.randint(1, 100)
 
+while True:
+    userChoice = int(input("Guess the taget : "))
+    if (userChoice == target):
+        print ("Success : correct Guess!!")
+        break
+    elif(userChoice < target):
+        print("Your number was too small. Take a bigger guess...")
+    else:
+        print("Your number was to big. Take a smaller guess..")
+        
+print("----Game Over----")        
